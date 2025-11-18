b1aa49054c3a7b80d0af7356faf90a4f26fd80c8
Khulood <khulood1027@gmail.com>
2025-11-18 13:06:21 +0000

    Delete Old Files

--- app/src/test/java/onlineshopping/BundleDiscountTest.java
+++ app/src/test/java/onlineshopping/BundleDiscountTest.java
@@ -13,5 +13,27 @@ import org.junit.jupiter.api.Disabled;
     * 3. one laptop, two mice
 */
 public class BundleDiscountTest {
-    
+    private DiscountService discountService;
+    private List<CartItem> cartItems;
+    private CustomerType customerType;
+    private CartItem laptop;
+    private CartItem mouse;
+
+    @BeforeEach
+    public void setUp() {
+        discountService = new DiscountService();
+        cartItems = new ArrayList<>();
+        customerType = CustomerType.REGULAR;
+    }
+
+    @Test
+    public void testOneMouseLaptopPair() {
+        laptop = new CartItem("Laptop", 1000.0, 1);
+        mouse = new CartItem("Mouse", 50.0, 1);
+        cartItems.add(laptop);
+        cartItems.add(mouse);
+        double discount = discountService.calculateBundleDiscount(cartItems, customerType);
+        assertEquals(50.0, discount, 0.01);
+
+
 }
